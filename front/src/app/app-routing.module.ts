ee709d5f714eab9b5199289282e760e19b4122b6
gonzalezgancedomaria <gonzalezgancedomaria@gmail.com>
2023-07-11 21:13:23 +0100

    menu y product display, añadir snackbar, detalles e inicio de ngrx

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookATableComponent } from './book-a-table/book-atable.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './menu/menu-component/menu.component';
 import { OrdersComponent } from './orders/orders.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './users/profile/profile.component';
+import { ShoppingCartComponent } from './cart/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +14,8 @@ const routes: Routes = [
   { path: 'menu', component: MenuComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'reviews', component: ReviewsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'cart', component: ShoppingCartComponent }
 ];
 
 @NgModule({
---
+++ front/src/store/actions/product/product.actions.ts
@@ -0,0 +1,5 @@
+import { createAction } from '@ngrx/store';
+
+
+export const addProductToTheCart = createAction('[Product] Add to the cart');
+export const addProductToTheFavouriteList = createAction('[Product] Add to the favourite list');
