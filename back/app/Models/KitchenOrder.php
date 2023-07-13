4e50478dfd1712188971597938560cf65a8056e6
Aritz Ibarra <aibarrauretxe@outlook.es>
2023-07-13 18:18:30 +0200

    (Fix) KitchenOrder relation with Table. Added missing relation.

--- back/app/Models/KitchenOrder.php
+++ back/app/Models/KitchenOrder.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KitchenOrder extends Model
@@ -26,5 +26,10 @@ class KitchenOrder extends Model
     public function employees():BelongsToMany
     {
         return $this->belongsToMany(Employee::class,'cook');
+    }
+
+    public function tables():BelongsToMany
+    {
+        return $this->belongsToMany(Employee::class,'table');
     }
 }
