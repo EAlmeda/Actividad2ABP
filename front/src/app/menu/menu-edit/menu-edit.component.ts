8c4ca752c2e5e8d79f7670ac29ee637921ecd48f
Eric Almeda <e.almeda.t@gmail.com>
2023-07-30 18:09:23 +0200

    Snackbar menu edit

--- front/src/app/menu/menu-edit/menu-edit.component.ts
+++ front/src/app/menu/menu-edit/menu-edit.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { Product } from 'src/models/Product';
 import { ProductService } from '../product.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-menu-edit',
@@ -14,7 +15,8 @@ export class MenuEditComponent implements OnInit {
   product: Product;
   constructor(
     private formBuilder: FormBuilder,
-    private productService: ProductService
+    private productService: ProductService,
+    private _snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -32,10 +34,29 @@ export class MenuEditComponent implements OnInit {
   submit() {
     this.productService.saveProduct(this.form.value).subscribe(
       (result) => {
-        console.log(result);
+        this._snackBar.open('Product added', 'Close', {
+          horizontalPosition: 'end',
+          verticalPosition: 'bottom',
+        });
+        this.form = this.formBuilder.group({
+          name: ['', [Validators.required]],
+          price: ['', [Validators.required]],
+          available: ['', [Validators.required]],
+          recipe: ['', [Validators.required]],
+          image_path: ['', [Validators.required]],
+          description: ['', [Validators.required]],
+          type: ['', [Validators.required]],
+        });
       },
       (error) => {
-        console.log(error);
+        this._snackBar.open(
+          'An error has occured during adding a product',
+          'Close',
+          {
+            horizontalPosition: 'end',
+            verticalPosition: 'bottom',
+          }
+        );
       }
     );
   }
